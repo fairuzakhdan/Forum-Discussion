e42c35765ec8698bb9df653d4cf8ffffa6ae2afd
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-01 01:26:31 +0700

    update reducer test

--- src/states/threads/reducer.test.js
+++ src/states/threads/reducer.test.js
@@ -1,9 +1,10 @@
 import { describe, expect, it } from 'vitest';
 import { threadsReducer } from './reducer';
 
-// talk reducer skenario
+// threads reducer skenario
 // - mengembalikan state kosong jika action unknown
-// - mengembalikan state data threads jika actionnya RECEIVE_THREADS
+// - mengembalikan data state threads jika actionnya RECEIVE_THREADS
+// - mengembalikan nilai baru pada state threads jika actionnya  ADD_THREAD dan payload thread
 
 describe('threads reducer', () => {
   it('should return the initial state when given by unknown action', () => {
@@ -13,22 +14,22 @@ describe('threads reducer', () => {
     const nextState = threadsReducer(initialState, action);
     expect(nextState).toEqual(initialState);
   });
-  it('should return the talks when given by RECEIVE_TALKS action', () => {
+  it('should return the threads when given by RECEIVE_THREADS action', () => {
     const initialState = [];
     const action = {
       type: 'RECEIVE_THREADS',
       payload: {
         threads: [
           {
-            id: 'thread-1',
-            title: 'Thread Pertama',
-            body: 'Ini adalah thread pertama',
-            category: 'General',
-            createdAt: '2021-06-21T07:00:00.000Z',
-            ownerId: 'users-1',
-            upVotesBy: [],
-            downVotesBy: [],
-            totalComments: 0,
+            'id': 'thread-1',
+            'title': 'Thread Pertama',
+            'body': 'Ini adalah thread pertama',
+            'category': 'General',
+            'createdAt': '2021-06-21T07:00:00.000Z',
+            'ownerId': 'users-1',
+            'upVotesBy': [],
+            'downVotesBy': [],
+            'totalComments': 0
           },
           {
             'id': 'thread-2',
@@ -41,7 +42,6 @@ describe('threads reducer', () => {
             'downVotesBy': [],
             'totalComments': 0
           }
-
         ],
       },
     };
