46516f7fc319205f4333025aac31cfe290d97fdc
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-01 01:36:32 +0700

    add unitest reducer add thread

--- src/states/threads/reducer.test.js
+++ src/states/threads/reducer.test.js
@@ -48,4 +48,37 @@ describe('threads reducer', () => {
     const nextState = threadsReducer(initialState, action);
     expect(nextState).toEqual(action.payload.threads);
   });
+  it('should return the threads with the new talk when given by ADD_THREAD action', () => {
+    const initialState = [
+      {
+        'id': 'thread-1',
+        'title': 'Thread Pertama',
+        'body': 'Ini adalah thread pertama',
+        'category': 'General',
+        'createdAt': '2021-06-21T07:00:00.000Z',
+        'ownerId': 'users-1',
+        'upVotesBy': [],
+        'downVotesBy': [],
+        'totalComments': 0
+      },
+    ];
+    const action = {
+      type: 'ADD_THREAD',
+      payload: {
+        thread: {
+          'id': 'thread-2',
+          'title': 'Thread Kedua',
+          'body': 'Ini adalah thread kedua',
+          'category': 'General',
+          'createdAt': '2021-06-21T07:00:00.000Z',
+          'ownerId': 'users-2',
+          'upVotesBy': [],
+          'downVotesBy': [],
+          'totalComments': 0
+        }
+      }
+    };
+    const nextState = threadsReducer(initialState, action);
+    expect(nextState).toEqual([...initialState, action.payload.thread]);
+  });
 });
