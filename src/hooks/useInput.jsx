e22ef120b4638195e8405c34c836f22516052900
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-05-04 02:36:22 +0700

    update

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -5,7 +5,7 @@ const useInput = (defaultValue = "") => {
   const onChange = (event) => {
     setValue(event.target.value);
   };
-  return [value];
+  return [value, onChange];
 };
 
 export default useInput;
